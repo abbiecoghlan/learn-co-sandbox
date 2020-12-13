895b14b64e0f7100968da193af598a4283e77616
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-13 21:08:21 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting_programmer(name = "Abbie", language = "Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer
+greeting_programmer("Abbie")
+greeting_programmer("Abigail", "Javascript")
